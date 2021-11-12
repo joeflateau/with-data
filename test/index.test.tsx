b1a429e864d797ab7f589c55235a4ce3cfe94e5d
Joe Flateau <joe@joeflateau.net>
2021-11-12 12:40:33 -0500

    test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { withDataItem } from '../src';
+
+const Button = (props: { onClick?: () => void; children: React.ReactNode }) => {
+  return <button onClick={props.onClick}>{props.children}</button>;
+};
 
 describe('Thing', () => {
   it('renders without crashing', () => {
+    const DataItemButton = withDataItem(
+      Button, // <---------- A component to wrap
+      ['onClick'] // <------ which event handlers should be wrapped
+    );
+
+    const people = ['Derek', 'Jim', 'Joe', 'Mike'];
+
+    const sayName = (name: string) => alert(`Hello, ${name}`);
+
+    const App = () => {
+      return (
+        <div>
+          {people.map((person) => (
+            <DataItemButton
+              dataItem={person} // <------------ associate any data with this component
+              onClickDataItem={sayName} // <---- no need for lambda here which would otherwise cause unnecessary rerenders, data item will be passed as the first arg of the handler
+            >
+              {person}
+            </DataItemButton>
+          ))}
+        </div>
+      );
+    };
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
